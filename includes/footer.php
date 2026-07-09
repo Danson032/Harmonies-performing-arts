e18912f4cb05986d9f88d17a0bf2652494a8a896
Danson Nganga <Dansonnganga019@gmail.com>
2026-07-09 11:34:40 +0300

    commit

---
+++ includes/footer.php
@@ -0,0 +1,37 @@
+<footer>
+
+    <div class="footer-content">
+
+        <h3>Harmonies Performing Arts</h3>
+
+        <p>
+            Inspiring musical excellence through professional choir training,
+            composition and performance.
+        </p>
+
+        <div class="socials">
+
+            <a href="#"><i class="fab fa-facebook-f"></i></a>
+
+            <a href="#"><i class="fab fa-instagram"></i></a>
+
+            <a href="#"><i class="fab fa-youtube"></i></a>
+
+            <a href="#"><i class="fab fa-whatsapp"></i></a>
+
+        </div>
+
+        <hr>
+
+        <p>
+            &copy; <?php echo date("Y"); ?>
+            Harmonies Performing Arts.
+            All Rights Reserved.
+        </p>
+
+    </div>
+
+</footer>
+
+</body>
+</html>
---
+++ includes/navbar.php
@@ -0,0 +1,33 @@
+<header>
+
+    <nav class="navbar">
+
+        <a href="index.php" class="logo">
+            Harmonies <span>Performing Arts</span>
+        </a>
+
+        <ul class="nav-menu">
+
+            <li><a href="index.php">Home</a></li>
+
+            <li><a href="about.php">About</a></li>
+
+            <li><a href="services.php">Services</a></li>
+
+            <li><a href="team.php">Team</a></li>
+
+            <li><a href="music.php">Music</a></li>
+
+            <li><a href="gallery.php">Gallery</a></li>
+
+            <li><a href="contact.php">Contact</a></li>
+
+        </ul>
+
+        <a href="contact.php" class="btn">
+            Book Training
+        </a>
+
+    </nav>
+
+</header>
